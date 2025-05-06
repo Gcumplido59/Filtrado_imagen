3c2e1f611e30678fc1c2fcdc8d5ef357789ba467
Gcumplido59 <gaelmendo59@gmail.com>
2025-05-06 07:54:27 -0600

    corrección de comentarios en código
    Se aplicaron cambios en los comentarios para un mejor entendimiento de las funcionalidades del codigo

--- Actividad_1_Filtrado_imagen.py
+++ Actividad_1_Filtrado_imagen.py
@@ -5,14 +5,14 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'     # Ruta a tesseract.exe
 
-# Cargar imágenes
+# Cargar imagenes de las placas con la libreria OpenCV
 imagen1 = cv2.imread('./placa_q.jpg')
 imagen2 = cv2.imread('./placa_2.jpg')
 
 
-img_achicado = imagen2[::2, ::2]        # Achicar imagen2
+img_achicado = imagen2[::2, ::2]        # Se achica la imagen 2 a la mitad de su tamaño
 
-# Aplicar blur gaussiano
+# Aplicacion de filtro gaussiano a la imagen 1 e imagen 2
 imagenblurgaussiano = cv2.GaussianBlur(imagen1, (35, 35), 0)
 imagenblurgaussiano2 = cv2.GaussianBlur(img_achicado, (35, 35), 0)
 
@@ -23,30 +23,33 @@ hsv2 = cv2.cvtColor(imagenblurgaussiano2, cv2.COLOR_BGR2HSV)        # Convertir 
 lower_black2 = np.array([0, 0, 186])
 upper_black2 = np.array([150, 80, 255])
 
-# Mascara imagen 2
+# Se crea mascara para la imagen 2
 mascara2 = cv2.inRange(hsv2, lower_black2, upper_black2)
 num_img2 = cv2.bitwise_and(imagenblurgaussiano2, imagenblurgaussiano2, mask=mascara2)
 
-# OCR
+# OCR (Reconocimiento de caracteres) para la imagen 1 y la imagen 2
+# Se define la funcion de procesamiento de imagenes para OCR
 def ocr_proceso(imagen):
     gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     _, umbral = cv2.threshold(gris, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return umbral
 
+# Procesar imagenes para OCR
 umbral1 = ocr_proceso(imagenblurgaussiano)
 umbral2 = ocr_proceso(num_img2)
 
-# Extraer texto con OCR
+# Extraer texto con OCR y la libreria pytesseract
+# Se utiliza el modo de segmentacion 6 (PSM 6) para una sola columna de texto
 texto1 = pytesseract.image_to_string(umbral1, config='--psm 6')
 texto2 = pytesseract.image_to_string(umbral2, config='--psm 6')
 
+# Mostrar resultados en consola de texto extraido de la imagen 1 y 2
 print("Texto detectado en imagen 1:")
 print(texto1)
-
 print("\nTexto detectado en imagen 2:")
 print(texto2)
 
-# Imagenes procesadas
+# Imagenes procesadas mostradas en ventana
 cv2.imshow('Imagen 1 - OCR', umbral1)
 cv2.imshow('Imagen 2 - OCR', umbral2)
 cv2.waitKey(0)
